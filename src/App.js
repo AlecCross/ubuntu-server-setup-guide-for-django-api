6082344cbc0b96d9c152829e5a23cf2766864125
AlecCross <cross2472@gmail.com>
2023-01-17 21:57:10 +0200

    Initial+Contents

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Contents from "./components/Contents";
+
+function App() {
+  return (
+    <Contents />
+  );
+}
+
+export default App;
---
+++ src/components/Contents.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Contents() {
+    return (
+        <ol>
+            <li>Вступ</li>
+            <li>Мета</li>
+            <li>Встановлення Linux Ubuntu</li>
+            <li>Налаштування мережі</li>
+            <li>Підключення по SSH</li>
+            <li>Завантаження проекту з github</li>
+            <li>Інсталяція потрібної версії Python</li>
+            <li>Інсталяція pip</li>
+            <li>Інсталяція virtual environment</li>
+            <li>Створення користувача (Для використання залежностей через pip)</li>
+            <li>Інсталяція Postgresql</li>
+            <li>Налаштування БД</li>
+            <li>Інсталяція та налаштування ctrbot/https sertificate/nginx</li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ol>
+    )
+}
